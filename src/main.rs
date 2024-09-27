5b030da6e3aa876f6a14f881da662d207a22dd53
Oscar Eriksson <oscareriksson2004@hotmail.com>
2024-09-27 02:21:56 +0200

    Merged a lot from own-lib branch so we now have better ui, board rotation, fixed textures and more

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use angun_chess::*;
 use other_functions::*;
 
+
 use raylib::prelude::*;
 use valid_moves::solves_check_move;
 
@@ -24,7 +25,7 @@ fn main() {
     
 
     // Content
-    let mut assets = ChessAssets::new(&mut rl, &thread, 2, 2);
+    let mut assets = ChessAssets::new(&mut rl, &thread, 2, 3);
     let circle = rl.load_texture(&thread, "assets/Circle.png").unwrap();
     let dot = rl.load_texture(&thread, "assets/Dot.png").unwrap();
     
@@ -33,17 +34,41 @@ fn main() {
 
     let mut positions = Vec::<(i32, i32)>::new();
     let mut selected_piece = String::new();
+    let mut rotated = false;
+
+    let mut pre_game = true;
+    let mut pre_game_menu = UIBox {
+        x: 0.0,
+        y: 0.0,
+        text: String::from("oscae-chess-gui"),
+        font_size: 12,
+        text_color: Color::BLACK,
+        width: 120.0,
+        height: 60.0,
+        color: Color::LIGHTGRAY,
+        outline_color: Color::BLACK,
+        buttons: vec![UIButton { x: 0.0, y: 10.0, text: String::from("PLAY"), font_size: 20, text_color: Color::BLACK, width: 100.0, height: 30.0, color: Color::WHITE, outline_color: Color::DARKGRAY }],
+    };
+    let mut post_game_menu = UIBox {
+        x: 0.0,
+        y: 0.0,
+        text: String::new(),
+        font_size: 15,
+        text_color: Color::BLACK,
+        width: 120.0,
+        height: 60.0,
+        color: Color::LIGHTGRAY,
+        outline_color: Color::BLACK,
+        buttons: vec![UIButton { x: 0.0, y: 10.0, text: String::from("continue"), font_size: 20, text_color: Color::BLACK, width: 100.0, height: 30.0, color: Color::WHITE, outline_color: Color::DARKGRAY }],
+    };
 
     while !rl.window_should_close() {
         
         // ------- Update ----------------------------------------
 
-
-
+        // gui logic
         let window_width = rl.get_screen_width() as f32;
         let window_height = rl.get_screen_height() as f32;
-
-        let rotated = false;
 
         let asset_size = assets.size as f32;
         let asset_square_size = assets.square_size as f32;
@@ -67,6 +92,12 @@ fn main() {
             window_height
         };
 
+        let center_x = window_width / 2.0;
+        let center_y = window_height / 2.0;
+        
+        let board_left = (window_width - board_size) / 2.0 + board_offset;
+        let board_top = (window_height - board_size) / 2.0 + board_offset;
+
         //let fps = rl.get_fps();
         
         if notification_time > -1.0 {
@@ -79,13 +110,54 @@ fn main() {
             notification_time = 1.0;
         }
 
+        if rl.is_key_pressed(KeyboardKey::KEY_F) {
+            rotated = !rotated;
+        }
 
-        let square_x = ((mouse_x - board_offset) / scale / asset_square_size).floor();
-        let square_y = ((mouse_y - board_offset) / scale / asset_square_size).floor();
 
-        if square_x >= 0.0 && square_x <= 7.0 && square_y >= 0.0 && square_y <= 7.0 {
+        let square_x = ((mouse_x - board_left) / board_square_size).floor();
+        let square_y = ((mouse_y - board_top) / board_square_size).floor();
+
+        if pre_game {
+            for button in &mut pre_game_menu.buttons {
+                if button.is_hovered(Vector2::new(center_x + pre_game_menu.x, center_y + pre_game_menu.y), scale, mouse_x, mouse_y) {
+
+                    button.color = Color::WHITE;
+
+                    if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+                        pre_game = false;
+                    }
+                }
+                else {
+                    button.color = Color::LIGHTGRAY;
+                }
+            }
+        }
+
+        if chess_eninge.game_over {
+            for button in &mut post_game_menu.buttons {
+                if button.is_hovered(Vector2::new(center_x + post_game_menu.x, center_y + post_game_menu.y), scale, mouse_x, mouse_y) {
+
+                    button.color = Color::WHITE;
+
+                    if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+                        pre_game = true;
+                        chess_eninge = ChessEngine::new();
+                    }
+                }
+                else {
+                    button.color = Color::LIGHTGRAY;
+                }
+            }
+        }
+        
+
+        // chess logic
+        if !pre_game && square_x >= 0.0 && square_x <= 7.0 && square_y >= 0.0 && square_y <= 7.0 {
             if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
-                let square = (square_x as i32, square_y as i32);
+                
+                let square = (mirror(square_x as i32, rotated), mirror(square_y as i32, rotated));
+
                 if positions.contains(&square) {
                     chess_eninge.do_move(&selected_piece, square);
                     positions.clear();
@@ -98,17 +170,17 @@ fn main() {
                         positions.push((index.1, index.0));
                     }
                 }
-
             }
         }
+
         // ------- Draw ------------------------------------------
         let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::CORNFLOWERBLUE);
+        d.clear_background(Color::BLACK);
 
         // board
         d.draw_texture_pro(&assets.board,
             Rectangle::new(0.0, 0.0, asset_size, asset_size),
-            Rectangle::new(window_width / 2.0, window_height / 2.0, board_size, board_size),
+            Rectangle::new(center_x, center_y, board_size, board_size),
             Vector2::new(board_size / 2.0, board_size / 2.0), rotation, Color::WHITE);
         
         // pieces
@@ -133,8 +205,8 @@ fn main() {
                 let source_rec = Rectangle::new(0.0, 0.0, asset_square_size, asset_square_size);
                 d.draw_texture_pro(piece_asset, source_rec,
                     Rectangle::new(
-                        window_width / 2.0 + board_square_size * (x as f32 - 3.5),
-                        window_height / 2.0 + board_square_size * (y as f32 - 3.5),
+                    board_left + board_square_size * (mirror(x as i32, rotated) as f32 + 0.5),
+                    board_top + board_square_size * (mirror(y as i32, rotated) as f32 + 0.5),
                         board_square_size, board_square_size),
                     Vector2::new(board_square_size / 2.0, board_square_size / 2.0), 0.0, Color::WHITE);
             }
@@ -151,8 +223,8 @@ fn main() {
             let source_rec = Rectangle::new(0.0, 0.0, asset_square_size, asset_square_size);
             d.draw_texture_pro(pos_texture, source_rec,
                 Rectangle::new(
-                    window_width / 2.0 + board_square_size * (pos.0 as f32 - 3.5),
-                    window_height / 2.0 + board_square_size * (pos.1 as f32 - 3.5),
+                    board_left + board_square_size * (mirror(pos.0, rotated) as f32 + 0.5),
+                    board_top + board_square_size * (mirror(pos.1, rotated) as f32 + 0.5),
                     board_square_size, board_square_size),
                 Vector2::new(board_square_size / 2.0, board_square_size / 2.0), 0.0, Color::new(128, 128, 128, 128));
         }
@@ -160,14 +232,29 @@ fn main() {
         // cursor
         if d.is_cursor_on_screen() {
             // highlight
-            if mouse_x > board_offset && mouse_x < board_offset + 8.0 * board_square_size &&
-                mouse_y > board_offset && mouse_y < board_offset + 8.0 * board_square_size {
+            if !pre_game && square_x >= 0.0 && square_x <= 7.0 &&
+            square_y >= 0.0 && square_y <= 7.0 {
 
-                d.draw_rectangle_rec(Rectangle::new(square_x * board_square_size + board_offset, square_y * board_square_size + board_offset, board_square_size, board_square_size), Color::new(150, 150, 150, 100));
+                d.draw_rectangle_rec(Rectangle::new(board_left + square_x * board_square_size, board_top + square_y * board_square_size, board_square_size, board_square_size), Color::new(150, 150, 150, 100));
             }
 
             // cursor
-            d.draw_circle(mouse_x as i32, mouse_y as i32, 5.0, Color::RED);
+            //d.draw_circle(mouse_x as i32, mouse_y as i32, 5.0, Color::RED);
+        }
+
+        // menu
+        if pre_game {
+            pre_game_menu.draw(&mut d, Vector2::new(center_x, center_y), scale);
+        }
+
+        if chess_eninge.game_over {
+            let t = match chess_eninge.whites_turn_to_move {
+                false => "White won",
+                true => "Black won",
+            };
+
+            post_game_menu.text = String::from(t);
+            post_game_menu.draw(&mut d, Vector2::new(center_x, center_y), scale);
         }
 
         // notification
@@ -186,6 +273,14 @@ fn main() {
                 ((topleft_y + 4.0) * scale) as i32,
                 font_size, Color::from(Color::BLACK))
         }
+    }
+}
+
+fn mirror(i: i32, mirror: bool) -> i32 {
+    if mirror {
+        7 - i
+    } else {
+        i
     }
 }
 
@@ -259,10 +354,10 @@ impl ChessAssets {
     }
 }
 
-
 struct ChessEngine {
     board: Vec<Vec<String>>,
     whites_turn_to_move: bool,
+    game_over: bool,
 }
 
 impl ChessEngine {
@@ -296,10 +391,12 @@ impl ChessEngine {
             board[6][i] = white_pieces.get(i + 8)
             .expect("Piece at index {i} does not exist in white_pieces").to_string();
         }
+        let game_over = false;
 
         Self {
             board,
             whites_turn_to_move,
+            game_over,
         }
     }
 
@@ -351,10 +448,90 @@ impl ChessEngine {
         //Change turns
         self.whites_turn_to_move = !self.whites_turn_to_move;
 
+        // calculate checkmate
+        if self.get_valid_pieces().len() == 0 {
+            self.game_over = true;
+        }
+
         true
     }
 
     fn get_piece(&self, (x, y): (i32, i32)) -> String {
         self.board[y as usize][x as usize].clone()
+    }
+}
+
+struct UIBox {
+    x: f32,
+    y: f32,
+
+    text: String,
+    font_size: i32,
+    text_color: Color,
+    width: f32,
+    height: f32,
+    color: Color,
+    outline_color: Color,
+    buttons: Vec<UIButton>
+}
+
+impl UIElement for UIBox {
+    fn draw(&self, d: &mut RaylibDrawHandle, origin: Vector2, scale: f32) {
+        d.draw_rectangle_rec(Rectangle::new(origin.x + (self.x - self.width / 2.0) * scale, origin.y + (self.y - self.height / 2.0) * scale, self.width * scale, self.height * scale), self.color);
+        d.draw_rectangle_lines_ex(Rectangle::new(origin.x + (self.x - self.width / 2.0) * scale, origin.y + (self.y - self.height / 2.0) * scale, self.width * scale, self.height * scale), scale * 2.0, self.outline_color);
+    
+        let text_width = d.measure_text(&self.text, self.font_size) as f32;
+
+        d.draw_text(&self.text,
+            (origin.x + (self.x - text_width / 2.0) * scale) as i32,
+            (origin.y + (self.y - self.height as f32 / 2.0) * scale) as i32,
+            (self.font_size as f32 * scale) as i32, self.text_color);
+
+        for button in &self.buttons {
+            button.draw(d, origin + Vector2::new(self.x, self.y), scale);
+        }
+    }
+}
+
+struct UIButton {
+    x: f32,
+    y: f32,
+
+    text: String,
+    font_size: i32,
+    text_color: Color,
+    width: f32,
+    height: f32,
+    color: Color,
+    outline_color: Color,
+}
+
+pub trait UIElement {
+    fn draw(&self, d: &mut RaylibDrawHandle, origin: Vector2, scale: f32);
+}
+
+impl UIElement for UIButton {
+    fn draw(&self, d: &mut RaylibDrawHandle, origin: Vector2, scale: f32) {
+        d.draw_rectangle_rec(Rectangle::new(origin.x + (self.x - self.width / 2.0) * scale, origin.y + (self.y - self.height / 2.0) * scale, self.width * scale, self.height * scale), self.color);
+        d.draw_rectangle_lines_ex(Rectangle::new(origin.x + (self.x - self.width / 2.0) * scale, origin.y + (self.y - self.height / 2.0) * scale, self.width * scale, self.height * scale), scale * 2.0, self.outline_color);
+        
+        let text_width = d.measure_text(&self.text, self.font_size) as f32;
+
+        d.draw_text(&self.text,
+            (origin.x + (self.x - text_width / 2.0) * scale) as i32,
+            (origin.y + (self.y - self.font_size as f32 / 2.0) * scale) as i32,
+            (self.font_size as f32 * scale) as i32, self.text_color);
+    }
+}
+
+impl UIButton {
+    fn is_hovered(&self, origin: Vector2, scale: f32, mouse_x: f32, mouse_y: f32) -> bool {
+        let left = origin.x + (self.x - self.width / 2.0) * scale;
+        let right = left + self.width * scale;
+
+        let top = origin.y + (self.y - self.height / 2.0) * scale;
+        let bottom = top + self.height * scale;
+        
+        left < mouse_x && mouse_x < right && top < mouse_y && mouse_y < bottom
     }
 }
